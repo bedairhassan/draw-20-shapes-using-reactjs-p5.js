428245797b83f01192089225ba10f75ac4100876
hankerhankk11 <55901623+hankerhankk11@users.noreply.github.com>
2019-12-25 10:23:40 +0200

    hanker

--- src/Q.js
+++ src/Q.js
@@ -1,24 +1,19 @@
 import React from 'react';
-const p5 = require('node-p5');
- 
-function Sketch(p) {
-    p.setup = () => {
-        p.createCanvas(200, 200);
-    }
-    p.draw = () => {
-        p.background(50);
-        p.text('hello world!', 50, 100);
-    }
-}
- 
-let p5Instance = p5.createSketch(Sketch);
 
-export default function App(){
+export default class Q extends React.Component{
 
-    return (
-        <center>
-        <h2>Q</h2>
-        {p5Instance}
-        </center>
-    )
+    constructor(props){
+
+        super(props)
+    }
+
+    render(){
+
+        return(
+
+            <center>
+                <h1>This is Q</h1>
+            </center>
+        )
+    }
 }
